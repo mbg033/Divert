941fa6c3552ad623295ba0611a8f8e7302d795d5
Ilya Kitaev <mbg033@gmail.com>
2017-03-28 14:59:43 +0300

    Proxy on loopback interface

--- examples/streamdump/streamdump.c
+++ examples/streamdump/streamdump.c
@@ -247,24 +247,29 @@ read_failed:
                 if (tcp_header->DstPort == htons(port))
                 {
                     // Reflect: PORT ---> PROXY
+					message("outbound PORT ---> PROXY");
                     UINT32 dst_addr = ip_header->DstAddr;
                     tcp_header->DstPort = htons(proxy_port);
-                    ip_header->DstAddr = ip_header->SrcAddr;
+                    // ip_header->DstAddr = ip_header->SrcAddr;
+					ip_header->DstAddr = htonl(INADDR_LOOPBACK);
                     ip_header->SrcAddr = dst_addr;
                     addr.Direction = WINDIVERT_DIRECTION_INBOUND;
                 }
                 else if (tcp_header->SrcPort == htons(proxy_port))
                 {
                     // Reflect: PROXY ---> PORT
-                    UINT32 dst_addr = ip_header->DstAddr;
+					message("outbound PROXY ---> PORT");
+					UINT32 dst_addr = ip_header->DstAddr;
                     tcp_header->SrcPort = htons(port);
                     ip_header->DstAddr = ip_header->SrcAddr;
                     ip_header->SrcAddr = dst_addr;
+					// ip_header->SrcAddr = htonl(INADDR_LOOPBACK);
                     addr.Direction = WINDIVERT_DIRECTION_INBOUND;
                 }
                 else if (tcp_header->DstPort == htons(alt_port))
                 {
                     // Redirect: ALT ---> PORT
+					message("outbount ALT ---> PORT");
                     tcp_header->DstPort = htons(port);
                 }
                 break;
@@ -273,6 +278,7 @@ read_failed:
                 if (tcp_header->SrcPort == htons(port))
                 {
                     // Redirect: PORT ---> ALT
+					message("outbount PORT ---> ALT");
                     tcp_header->SrcPort = htons(alt_port);
                 }
                 break;
@@ -337,6 +343,8 @@ static DWORD proxy(LPVOID arg)
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(proxy_port);
+	addr.sin_addr.S_un.S_addr = htonl(INADDR_LOOPBACK);
+
     if (bind(s, (SOCKADDR *)&addr, sizeof(addr)) == SOCKET_ERROR)
     {
         error("failed to bind socket (%d)", WSAGetLastError());
